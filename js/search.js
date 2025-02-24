8e8847804fb2bcc9786c1cf5713300ada9bdb347
Komalpreet001-coder <karrakp991@gmail.com>
2025-02-24 10:27:23 -0800

    new

--- js/search.js
+++ js/search.js
@@ -1,1 +1,8 @@
 const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventlistener('click', ()=> {
+    search.classList.toggle('active')
+    input.focus()
+})
